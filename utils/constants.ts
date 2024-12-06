98f60a7e7b791f74afdb4ae899f580d55abec84a
Andrew <andrew@leftclick.io>
2024-12-06 22:58:37 +0200

    1. Modified ION contract addresses.

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,10 +5,10 @@ export const PARAMS: IParams = {
             "main": {
                 "getGasUrl": "https://gbsc.blockscan.com/gasapi.ashx?apikey=key&method=gasoracle",
                 "explorerUrl": "https://bscscan.com/token/<ADDRESS>",
-                "wTonAddress": "0x6BFc16f717Da0868f7Cf8Fb7AdFAa1402710182c", // "0x1B615Fb55d329b19C2d3f56943a1D972EFc717a3", // ION test one deployment
-                "tonBridgeAddress": "Ef9mEAB-6__sWw41PbsYvbqEcWKY0evP3joL8YUSTDI_GSQA",
+                "wTonAddress": "0x6BFc16f717Da0868f7Cf8Fb7AdFAa1402710182c",
+                "tonBridgeAddress": "Ef_IkSbsYpMv8ZO6XFMsfIoqTn9OSRA-BU5-rKxIh_xfY0fz",
                 "tonCollectorAddress": "EQB91-zbCZDGGtVngKdyXB7Cnbm_DKVXJQsHyzDn3cZSFEuI",
-                "tonMultisigAddress": "kf89w3U1JBzTACgoqdeyELpHTq2I3V-uMXJoVrtn70tF8_lZ",
+                "tonMultisigAddress": "kf-ARFxMJ5AecAQFJoXf6HOZm1IMAlFGyF3rEkl6GXrr_7QD",
                 "tonCenterUrl": "https://api.testnet.ice.io/http/v2/jsonRPC",
                 "chainId": 56,
                 "blocksConfirmations": 12,
@@ -19,10 +19,10 @@ export const PARAMS: IParams = {
             "test": {
                 "getGasUrl": "https://gbsc.blockscan.com/gasapi.ashx?apikey=key&method=gasoracle",
                 "explorerUrl": "https://testnet.bscscan.com/token/<ADDRESS>",
-                "wTonAddress": "0x6BFc16f717Da0868f7Cf8Fb7AdFAa1402710182c", // "0x1B615Fb55d329b19C2d3f56943a1D972EFc717a3", // ION test one deployment
-                "tonBridgeAddress": "Ef9mEAB-6__sWw41PbsYvbqEcWKY0evP3joL8YUSTDI_GSQA",
+                "wTonAddress": "0x6BFc16f717Da0868f7Cf8Fb7AdFAa1402710182c",
+                "tonBridgeAddress": "Ef_IkSbsYpMv8ZO6XFMsfIoqTn9OSRA-BU5-rKxIh_xfY0fz",
                 "tonCollectorAddress": "EQB91-zbCZDGGtVngKdyXB7Cnbm_DKVXJQsHyzDn3cZSFEuI",
-                "tonMultisigAddress": "kf89w3U1JBzTACgoqdeyELpHTq2I3V-uMXJoVrtn70tF8_lZ",
+                "tonMultisigAddress": "kf-ARFxMJ5AecAQFJoXf6HOZm1IMAlFGyF3rEkl6GXrr_7QD",
                 "tonCenterUrl": "https://api.testnet.ice.io/http/v2/jsonRPC",
                 "chainId": 97,
                 "blocksConfirmations": 12,
