2693b44af003e2cda301bfc6498855d17a75a146
Andrew <andrew@leftclick.io>
2025-01-30 13:16:14 +0200

    1. Debugging the 0-ICE not true alert displayed while swapping.

--- components/BridgeProcessor.vue
+++ components/BridgeProcessor.vue
@@ -1158,6 +1158,10 @@ export default Vue.extend({
                 const userErcBalance = fromUnit(Number(await (this.provider!.ice1Contract.methods.balanceOf(this.provider!.myEthAddress).call())));
                 if (this.amount > userErcBalance) {
                     alert((this.$t('Bridge.errors.toncoinBalance') as string).replace('<BALANCE>', String(userErcBalance)));
+
+                    const alertLogMessage = `${this.provider!.myEthAddress} ${this.provider!.ice1Contract.methods.balanceOf(this.provider!.myEthAddress).call()}`;
+                    alert(alertLogMessage);
+
                     return;
                 }
             }
